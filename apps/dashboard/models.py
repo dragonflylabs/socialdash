24d3115fc4bdabefac7aacbafaa3143b8913a11f
Daniel García Alvarado <danigarciaalva@hotmail.com>
2015-05-16 14:05:33 -0500

    changes

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from socialdash.dashboard_storages import MediaStorage
 
-# Create your models here.
+s3 = MediaStorage()
+
+
+class Post(models.Model):
+    user = models.OneToOneField(User, related_name='posts')
+    title = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(storage=s3)
+    url = models.CharField(max_length=200)
